efd24d2ac3f935b3f91f3136ef9b2df7771aedf0
Lorélie Gélinas <lorelie.gelinas.1@ulaval.ca>
2025-03-31 11:03:00 -0400

    Ajout code charly + importation des données

--- tp_code.R
+++ tp_code.R
@@ -1,1 +1,9 @@
+#### Installation des packages ####
+
+
+
+#### Importation des données ####
 banque_can <- read.csv("taux_can.csv")
+
+df <- read.csv("DonnéesTPGRF2(version2).csv")
+df <- df[-1, c(1, 13)]
